2cfc11da3f27df3107cdd58e7651e40faccf423f
Ubuntu <ubuntu@ip-172-31-92-114.ec2.internal>
2019-11-22 23:44:55 +0000

    Update Client

--- src/Model/Client.java
+++ src/Model/Client.java
@@ -2,6 +2,10 @@ package Model;
 
 import java.util.ArrayList;
 
+package Model;
+
+import java.util.ArrayList;
+
 public class Client {
 	//attributes
 	private ArrayList<Order> orders;
@@ -10,9 +14,40 @@ public class Client {
 	private String address;
 	
 	//constructors
-
+	public Client(String name, Type type, String address){
+		this.name=name;
+		this.type=type;
+		this.address=address;
+	}
+	public setName(String name){
+		this.name=name;
+	}
+	public setType(Type type){
+		this.tye=type;
+	}
+	public setAddress(String address){
+		this.address=address;
+	}
+	public getName(){
+		return name;
+	}
+	public getType(){
+		return type;
+	}
+	public getAddress(){
+		return address;
+	}
+	public getClientInfo(String name){
+		
+	}
 	//methods
 	public compareTo() {};
 	public String toString() {};
 
+// Updating a client
+// Printing client information based on their name
+// Returning prior order information (if any) of a specific client
+// Printing the full list of all clients.
+
 }
+
