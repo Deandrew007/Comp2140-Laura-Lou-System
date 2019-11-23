e8eeb74d90c9acde13ac1b2794f8005c1fe4663e
Ubuntu <ubuntu@ip-172-31-92-114.ec2.internal>
2019-11-23 01:55:26 +0000

    Updated client info with getters, setters and other methods

--- src/Model/Client.java
+++ src/Model/Client.java
@@ -1,53 +1,61 @@
 package Model;
 
-import java.util.ArrayList;
+import java.util.*;
 
-package Model;
-
-import java.util.ArrayList;
-
-public class Client {
+public class Client implements Comparable<Client> {
 	//attributes
-	private ArrayList<Order> orders;
-	private String name;
-	private Type type;
-	private String address;
+	private ArrayList<Order> orders; //array list of orders //when user creates order it is sent into this list of orders (make function)
+	private String name; //String attribute for name
+	private Type type; //attribute storing enum type
+	private String address; //String attribute for address
+	HashMap<String, String> client=new HashMap<String, String>(); //storing string (name) to client_info eg. Bob = Address, Type 
 	
-	//constructors
 	public Client(String name, Type type, String address){
+		//initializes attributes to user original input
 		this.name=name;
 		this.type=type;
 		this.address=address;
 	}
-	public setName(String name){
+	public void setName(String name){
+		//sets name of client to user input
 		this.name=name;
 	}
-	public setType(Type type){
-		this.tye=type;
+	public void setType(Type type){
+		//sets order type of client to user input
+		this.type=type;
 	}
-	public setAddress(String address){
+	public void setAddress(String address){
+		//sets address of client to user input
 		this.address=address;
 	}
-	public getName(){
+	public String getName(){
 		return name;
 	}
-	public getType(){
+	public Type getType(){
 		return type;
 	}
-	public getAddress(){
+	public String getAddress(){
+		//returns address of client
 		return address;
 	}
-	public getClientInfo(String name){
-		
+	public String toString(String name){
+		//printing client information based on their name
+		System.out.println("Information on Client ",getName());
+		System.out.println("Address of Client: ",getAddress());
+		System.out.println("Type of Order: ",getType());
 	}
-	//methods
-	public compareTo() {};
-	public String toString() {};
-
-// Updating a client
-// Printing client information based on their name
+	public compareTo(Client other) {
+		//compares current client to another client by name
+		name1=getName();
+		name2=other.getName();
+		return (name1.compareTo(name2));
+	}
+	/**public String addClient(){
+		//Having HashMap of client information
+		client.put(getName(),getAddress(),getType().toString()) //associates name of client to its address and order type
+	}**/
+	public createOrder(String garment, int quantity, int size, String colour, int refNum, Calendar recDate, double payment, Status paymentStatus, int skillRequired){
+		orders.add(order1);
+	}
 // Returning prior order information (if any) of a specific client
-// Printing the full list of all clients.
-
 }
-
